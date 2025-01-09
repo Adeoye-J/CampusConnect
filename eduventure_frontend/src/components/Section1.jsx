c8f18e7c33c8eda364bde2d82251853ee3571f98
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-01-09 21:50:02 +0100

    Campus Connect commit at 1/9/2025, 9:50:02 PM

--- eduventure_frontend/src/components/Section1.jsx
+++ eduventure_frontend/src/components/Section1.jsx
@@ -3,14 +3,14 @@ import section1 from "/home/lodge12.jpg"
 
 const Section1 = () => {
     return (
-        <div className='flex px-12 py-32 m-4'>
-            <div className="flex-1 flex items-center justify-center">
-                <img src={section1} alt="" className='w-96 rounded-xl' />
+        <div className='flex px-12 py-32 m-4 gap-10'>
+            <div className="w-2/5 flex items-center justify-center">
+                <img src={section1} alt="" className='rounded-xl' />
             </div>
-            <div className="flex-1 flex flex-col gap-7 items-start justify-center">
-                <h2 className='text-7xl font-semibold'>Empower Your <span className='text-tertiary'>Campus Experience</span></h2>
-                <h1></h1>
-                <h1></h1>
+            <div className="w-3/5 flex flex-col gap-7 items-start justify-center">
+                {/* <h2 className='text-7xl font-semibold'>Empower Your <span className='text-tertiary'>Campus Experience</span></h2> */}
+                <h1 className='text-5xl md:text-5xl xl:text-7xl font-semibold'>Empower Your</h1>
+                <h1 className='text-5xl md:text-5xl xl:text-7xl font-semibold text-tertiary'>Campus Experience</h1>
                 <p className='text-gray-700 text-lg font-semibold'>Discover, connect, and thrive with fellow students!</p>
                 <button className='border border-black px-10 py-2 rounded-full font-semibold hover:border-tertiary hover:text-tertiary hover:scale-105 transition-all duration-500'>Explore More</button>
             </div>
