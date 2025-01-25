e964a7011aa5b9048498f398bcf015468fe0954e
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-01-25 16:15:02 +0100

    Campus Connect commit at 1/25/2025, 4:15:02 PM

--- eduventure_frontend/src/components/Benefits.jsx
+++ eduventure_frontend/src/components/Benefits.jsx
@@ -28,7 +28,7 @@ const Benefits = () => {
         <div className='bg-primary px-5 sm:px-3 md:px-12 py-12 lg:px-32 lg:py-24 sm:m-4 sm:rounded-lg max-sm:mt-1'>
             <span className='max-sm:text-xs px-4 md:px-6 py-2 border border-white rounded-full text-white'>Key Features</span>
             <h2 className='text-center mb-5 md:mb-10 text-2xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-7xl font-medium text-white mt-4'>Why Choose Us?</h2>
-            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5">
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5  ">
                 {
                     features.map((item, index) => (
                         <div key={index} className="group">
@@ -36,7 +36,7 @@ const Benefits = () => {
                                 <p className='p-2 md:p-3 px-4 rounded-t-2xl bg-tertiary font-medium text-sm md:text-lg w-2/3 text-white text-nowrap'>{item.section}</p>
                                 <div className="p-3 rounded-tr-2xl bg-white" />
                             </div>
-                            <div className="border rounded-2xl bg-white px-5 md:px-10 py-4 md:py-8 min-h-[300px] flex flex-col gap-3 justify-between items-start">
+                            <div className="border rounded-2xl bg-white px-5 py-4 md:py-8 min-h-[300px] flex flex-col gap-3 justify-between">
                                 <div className="font-medium flex flex-col items-start gap-3">
                                     <h3 className='text-lg md:text-2xl'>{item.title}</h3>
                                     <p className='text-sm text-[#333]'>{item.content}</p>
