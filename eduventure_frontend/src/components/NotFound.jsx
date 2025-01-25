6f86ab7474857ee6ac6eb09da1bb6744363d4169
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-01-25 16:40:02 +0100

    Campus Connect commit at 1/25/2025, 4:40:02 PM

--- eduventure_frontend/src/components/NotFound.jsx
+++ eduventure_frontend/src/components/NotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import notfound from "/not_found/notfound.jpeg"
 
 const NotFound = () => {
     return (
-        <div>
-            Page not found
+        <div className='flex '>
+            <div className="">
+                <img src={notfound} alt="" />
+                <h1>404</h1>
+                <h2>Page Not Found</h2>
+                <p>The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
+            </div>
         </div>
     )
 }
