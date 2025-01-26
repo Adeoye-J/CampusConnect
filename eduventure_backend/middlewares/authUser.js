3e565b101f6082fc9101424996c8a8dee7ac45f1
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-01-26 10:10:02 +0100

    Campus Connect commit at 1/26/2025, 10:10:02 AM

--- eduventure_backend/middlewares/authUser.js
+++ eduventure_backend/middlewares/authUser.js
@@ -7,12 +7,14 @@ const authUser = async (req, res, next) => {
         const {token} = req.headers;
         
         if (!token) {
-            return res.status(401).json({success: false, message: "Please login to access this route"});
+            return res.status(401).json({success: false, message: "Not Authorized, Login again"});
         }
 
         const decoded_token = jwt.verify(token, process.env.JWT_SECRET);
 
         req.body.userId = decoded_token.id;
+
+        next();
     } catch (error) {
         console.log(error);
         res.status(401).json({success: false, message: error.message});
