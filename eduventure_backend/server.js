6ca99cea01d8420142571a5d66cc665a38c4c7e3
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-02-06 21:10:04 +0100

    Campus Connect commit at 2/6/2025, 9:10:02 PM

--- eduventure_backend/server.js
+++ eduventure_backend/server.js
@@ -15,6 +15,6 @@ app.use(cors());
 // Routes
 app.use('/api/user', userRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => console.log(`Server running and listening on port ${PORT}`));
